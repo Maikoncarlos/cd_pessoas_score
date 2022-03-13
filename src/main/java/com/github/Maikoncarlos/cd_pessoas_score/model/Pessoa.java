f16fe420a6ef690e64e1788f56637993ac3113b1
Maikoncarlos <maikonsaturno@gmail.com>
2022-03-13 20:39:56 -0300

    Entidades criadas juntamente com os repository com JPA

--- src/main/java/com/github/Maikoncarlos/cd_pessoas_score/model/Pessoa.java
+++ src/main/java/com/github/Maikoncarlos/cd_pessoas_score/model/Pessoa.java
@@ -1,30 +1,39 @@
 package com.github.maikoncarlos.cd_pessoas_score.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
 import java.io.Serializable;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
+@Table(name = "tb_pessoa")
 public class Pessoa implements Serializable {
     private static final long serialVersionUID = 1L;
 
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Id
-    private Integer id;
+    private Long id;
 
+    @NotBlank
+    @Column(name = "nome")
     private String nome;
-
+    @NotBlank
+    @Column(name = "telefone")
+    private String telefone;
+    @Column(name = "idade")
     private int idade;
-
+    @Column(name = "cidade")
     private String cidade;
-
+    @Column(name = "estado")
     private String estado;
-
+    @NotBlank
+    @Column(name = "score")
     private int score;
 
 }
---
+++ src/main/resources/import.sql
@@ -0,0 +1,9 @@
+INSERT INTO tb_pessoa (nome, telefone, idade, cidade, estado, score) VALUES ('Maikon', '11 9876-5431',  32, 'São Paulo', 'São Paulo',550);
+INSERT INTO tb_pessoa (nome, telefone, idade, cidade, estado, score) VALUES ('Sonia',  '11 9876-5432',  65, 'São Paulo', 'São Paulo',850);
+INSERT INTO tb_pessoa (nome, telefone, idade, cidade, estado, score) VALUES ('Wagner', '11 9876-5433',  62, 'São Paulo', 'São Paulo',250);
+
+
+INSERT INTO tb_score (score_Descricao, score_Inicial, score_Final) VALUES ('Insuficiente', 0, 200);
+INSERT INTO tb_score (score_Descricao, score_Inicial, score_Final) VALUES ('Inaceitável', 201, 500);
+INSERT INTO tb_score (score_Descricao, score_Inicial, score_Final) VALUES ('Aceitável', 501, 700);
+INSERT INTO tb_score (score_Descricao, score_Inicial, score_Final) VALUES ('Recomendável', 701, 1000);
